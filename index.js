0cfad563692b23277697484849170e4e20dcc79e
Grant Cloyd <dgcloyd@gmail.com>
2021-05-18 13:14:52 -0500

    Working fine everywhere except learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = "that steve guy"
+
+
+//upper case function refuses to acknowledge 'bob', have passed this in console and have even set it to return 'BOB' and still it does not register
+function upperCaseCustomerName() {
+ let capital = customerName.toUpperCase(); 
+ return capital;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+} 
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "maybe bob"
+} 
